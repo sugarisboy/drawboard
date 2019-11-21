42c1ae7efcf0481486b4b97498bd9e99a8ab7aab
sugarisboy <sugarisboy@outlook.com>
2019-11-21 11:30:13 +0300

    board init

--- src/main/java/ru/englishcraft/drawboard/config/Config.java
+++ src/main/java/ru/englishcraft/drawboard/config/Config.java
@@ -1,5 +1,16 @@
 package ru.englishcraft.drawboard.config;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Getter;
+import ru.englishcraft.drawboard.models.Board;
+
+import java.util.List;
+
 public class Config extends ConfigAdapter {
 
+    @Expose
+    @SerializedName("boards")
+    @Getter
+    private List<Board> boards;
 }
---
+++ src/main/java/ru/englishcraft/drawboard/models/Board.java
@@ -0,0 +1,59 @@
+package ru.englishcraft.drawboard.models;
+
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import lombok.Data;
+import org.bukkit.Location;
+import org.bukkit.block.Block;
+import ru.englishcraft.drawboard.DrawBoard;
+import ru.englishcraft.drawboard.config.Config;
+
+import java.util.List;
+
+@Data
+public class Board {
+
+    @Expose
+    @SerializedName("position1")
+    private Location p1;
+
+
+    @Expose
+    @SerializedName("position2")
+    private Location p2;
+
+    public boolean isContains(Block block) {
+        Location location = block.getLocation();
+        if (!p1.getWorld().equals(p2.getWorld()))
+            return false;
+
+        if (!location.getWorld().equals(p1.getWorld()))
+            return false;
+
+        return
+            between(p1.getBlockX(), p2.getBlockX(), location.getBlockX()) &&
+            between(p1.getBlockY(), p2.getBlockY(), location.getBlockY()) &&
+            between(p1.getBlockZ(), p2.getBlockZ(), location.getBlockZ());
+    }
+
+    // A <= C <= B or B <= C <= A
+    private boolean between(int a, int b, int c) {
+        return a <= c && c <= b || b <= c && c <= a;
+    }
+
+    public Board create() {
+        Config config = DrawBoard.getInstance().config();
+        List<Board> boards = config.getBoards();
+        boards.add(this);
+        config.save();
+
+        return this;
+    }
+
+    public void delete() {
+        Config config = DrawBoard.getInstance().config();
+        List<Board> boards = config.getBoards();
+        boards.remove(this);
+        config.save();
+    }
+}
